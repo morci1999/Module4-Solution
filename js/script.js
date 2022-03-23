eded79f14c607abff8a9ee8fd1adcb50f4297a20
Morci1999 <amirhoseinje99@gmail.com>
2022-03-24 04:21:58 +0430

    file edites

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Ali", "Javad", "Jeddi", "Hossein", "John", "Ghasem", "Fatemeh", "Jamshid", "kowsar", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
@@ -13,4 +13,4 @@
       }
     }
     
-    })()
+    })();
